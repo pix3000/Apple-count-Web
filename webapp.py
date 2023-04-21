1769e12999572ad64c0c4a8f9e56a556f7b36476
H.J <51011169+pix3000@users.noreply.github.com>
2023-04-21 15:19:45 +0900

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -9,29 +9,59 @@ from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
 
-DATETIME_FORMAT = "%Y-%m-%d_%H-%M-%S-%f"
-
+DATETIME_FORMAT = "%Y-%m-%d_%H-%M"
 
 @app.route("/", methods=["GET", "POST"])
 def predict():
-    if request.method == "POST":
-        if "file" not in request.files:
+    if request.method == "POST": 
+        if "file1" not in request.files:
             return redirect(request.url)
-        file = request.files["file"]
-        if not file:
+        file1 = request.files["file1"]
+        if not file1:
+            return
+        
+        if "file2" not in request.files:  
+            return redirect(request.url)
+        file2 = request.files["file2"]
+        if not file2:
             return
 
-        img_bytes = file.read()
-        img = Image.open(io.BytesIO(img_bytes))
-        results = model([img])
+        front_bytes = file1.read()
+        back_bytes = file2.read()
+        #print(file)         <FileStorage: 'test.jpg' ('image/jpeg')>
 
-        results.render()  # updates results.imgs with boxes and labels
-        now_time = datetime.datetime.now().strftime(DATETIME_FORMAT)
-        img_savename = f"static/{now_time}.png"
-        Image.fromarray(results.ims[0]).save(img_savename)
-        return redirect(img_savename)
+        f_img = Image.open(io.BytesIO(front_bytes))
+        f_results = model([f_img])
 
+        b_img = Image.open(io.BytesIO(back_bytes))
+        b_results = model([b_img])
+
+
+        '''
+        print(f"front: {f_results}") #image 1/1: 613x960 60 apples
+                                     #Speed: 23.9ms pre-process, 667.5ms inference, 1.3ms NMS per image at shape (1, 3, 416, 640)
+        '''
+
+        f_count =  str(f_results).split()[-17] #Count number of apples
+        b_count =  str(b_results).split()[-17]
+
+        all_count =  int((int(f_count) + int(b_count)) * 1.8)
+        print(all_count)
+
+        f_results.render()
+        real_time = datetime.datetime.now().strftime(DATETIME_FORMAT)
+        f_img_savename = f"result/front/{real_time}.png"
+        Image.fromarray(f_results.ims[0]).save(f_img_savename)
+
+
+        b_results.render() 
+        b_img_savename = f"result/back/{real_time}.png"
+        Image.fromarray(b_results.ims[0]).save(b_img_savename)
+
+        return render_template('index.html', all_count = all_count)
+    
     return render_template("index.html")
+
 
 
 if __name__ == "__main__":
@@ -39,9 +69,6 @@ if __name__ == "__main__":
     parser.add_argument("--port", default=5000, type=int, help="port number")
     args = parser.parse_args()
 
-    #model = torch.hub.load('ultralytics/yolov5', 'yolov5s', pretrained=True)  
     model = torch.hub.load("/home/hj/yolov5", 'custom', path='/home/hj/yolov5/runs/train/exp2/weights/best.pt', source='local')
     model.eval()
-    app.run(host="0.0.0.0", port=args.port) 
-
-
+    app.run(host="0.0.0.0", port=args.port)
